8d0cc3f086a47a1c04c3ee3b4ac68e415e86069f
AnastasMIPT <AtosBel@ya.ru>
2021-04-11 21:34:20 +0300

    gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anastas/git/TarantoolWorks/PrintTarantool
-# Build directory: /home/anastas/git/TarantoolWorks/PrintTarantool/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(PrintMsgPuckTest.test "/home/anastas/git/TarantoolWorks/PrintTarantool/build/PrintMsgPuckTest.test")
