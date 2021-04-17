48a95639bc4df44afc33456660ef706eacd6c7e4
AnastasMIPT <AtosBel@ya.ru>
2021-04-17 13:03:53 +0300

    Added tests with complicated arrays

--- test/GetMsgPuckSubArray.cpp
+++ test/GetMsgPuckSubArray.cpp
@@ -50,11 +50,65 @@ void TestSimpleArray () {
 
         fail_if (strncmp (printed_data, "[\"test str\", \"somth\", 98]", 26) != 0);
     }
+}
+
+
+void TestComplicatedArray () {
+    TEST_INIT (0);
+    char data[BUFSIZ] = {};
+    char result[BUFSIZ] = {};
+    char printed_data[BUFSIZ] = {};
     
+    char* ptr = data;
+    ptr = mp_encode_array (ptr, 7);
+        ptr = mp_encode_str (ptr, "test str", 9);
+        ptr = mp_encode_str (ptr, "somth", 6);
+        ptr = mp_encode_uint (ptr, 27);
+        ptr = mp_encode_array (ptr, 3);
+            ptr = mp_encode_uint (ptr, 265);
+            ptr = mp_encode_uint (ptr, 38);
+            ptr = mp_encode_str (ptr, "dog", 4);
+        ptr = mp_encode_str (ptr, "some str", 9);
+        ptr = mp_encode_array (ptr, 2);
+            ptr = mp_encode_array (ptr, 2);
+                ptr = mp_encode_uint (ptr, 32);
+                ptr = mp_encode_uint (ptr, 123456);
+            ptr = mp_encode_uint (ptr, 35);
+        ptr = mp_encode_uint (ptr, 98);
+
+
+    TEST_CASE ("fields 0 3 4 6"); {
+        uint32_t filed_no[] = {0, 3, 4, 6};
+
+        const char* data_p = data;
+
+        get_subarr_from_mp_array (data_p, result, filed_no, 4, NULL);
+        const char* result_p = result;
+        char* printed_data_p = printed_data;
+        sprint_array (&result_p, 0, &printed_data_p);
+        printf ("prited data = *%s*\n", printed_data);
+
+        fail_if (strncmp (printed_data, "[\"test str\", [265, 38, \"dog\"], \"some str\", 98]", 47) != 0);
+    }
+    
+
+    TEST_CASE ("fields 2 4 5"); {
+        uint32_t filed_no[] = {2, 4, 5};
+
+        const char* data_p = data;
+
+        get_subarr_from_mp_array (data_p, result, filed_no, 3, NULL);
+        const char* result_p = result;
+        char* printed_data_p = printed_data;
+        sprint_array (&result_p, 0, &printed_data_p);
+        printf ("prited data = *%s*\n", printed_data);
+
+        fail_if (strncmp (printed_data, "[27, \"some str\", [[32, 123456], 35]]", 37) != 0);
+    }
 }
 
 int main () {
     TestSimpleArray ();
-
+    TestComplicatedArray ();
     return 0;
 }
--- test/PrintMsgPuckTest.cpp
+++ test/PrintMsgPuckTest.cpp
@@ -54,7 +54,7 @@ void TestPrintArray () {
 
         fail_if (strncmp (printed_data, "[\"test str\", \"somth\", 27]", 26) != 0);
     }
-    TEST_CASE ("With inner array");
+    TEST_CASE ("With inner uint array");
     {   
         memchr (data, 0, BUFSIZ);
         char* ptr = data;
@@ -70,6 +70,25 @@ void TestPrintArray () {
     
         printf ("prited data = *%s*\n", printed_data);
         fail_if (strncmp (printed_data, "[27, [24, 123]]", 16) != 0);
+    }
+    TEST_CASE ("With inner uint/str array"); {
+        memchr (data, 0, BUFSIZ);
+        char* ptr = data;
+        ptr = mp_encode_array (ptr, 4);
+        ptr = mp_encode_str (ptr, "test str", 9);
+        ptr = mp_encode_array (ptr, 3);
+            ptr = mp_encode_uint (ptr, 265);
+            ptr = mp_encode_uint (ptr, 38);
+            ptr = mp_encode_str (ptr, "dog", 4);
+        ptr = mp_encode_str (ptr, "some str", 9);
+        ptr = mp_encode_uint (ptr, 98);
+        
+        const char* data_p = data;
+        char* printed_data_p = printed_data;
+        sprint_array (&data_p, 0, &printed_data_p);
+    
+        printf ("prited data = *%s*\n", printed_data);
+        fail_if (strncmp (printed_data, "[\"test str\", [265, 38, \"dog\"], \"some str\", 98]", 47) != 0);
     }
 }
 
