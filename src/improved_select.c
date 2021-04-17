933bc916dc17717fdaca07f21bb85b21a6be157d
AnastasMIPT <AtosBel@ya.ru>
2021-04-17 13:56:30 +0300

    Clear code

--- src/improved_select.c
+++ src/improved_select.c
@@ -8,13 +8,10 @@
 
 const uint32_t MaxKeyFieldsInTuple   = 10;
 
-
-
 struct tnt_stream* select_some_after_key (struct tnt_stream* s, uint32_t space_id,
                                             uint32_t limit, struct tnt_stream* key, struct tnt_reply* reply) {
     
-    // printf ("From %s, %d\n", __func__, __LINE__);
-    ssize_t result = tnt_select (s, space_id, 0, limit, 0, TNT_ITER_GT, key);  // TODO interator GT after first request?  
+    ssize_t result = tnt_select (s, space_id, 0, limit, 0, TNT_ITER_GT, key);  
     tnt_flush (s);
 
     const char* key_data = TNT_SBUF_DATA(key);
@@ -29,8 +26,7 @@ struct tnt_stream* select_some_after_key (struct tnt_stream* s, uint32_t space_i
         return NULL;
         exit (1);
     }
-    // printf ("From %s, %d\n", __func__, __LINE__);
-    
+
     return get_new_key_by_reply (s, space_id, reply);
 }
 
@@ -46,8 +42,6 @@ struct tnt_stream* select_some_first (struct tnt_stream* s, uint32_t space_id, u
     tnt_flush (s);
     tnt_stream_free (tuple);
 
-    // printf ("From %s, %d\n", __func__, __LINE__);
-    
     tnt_reply_init (reply);
     s->read_reply (s, reply);
     if (reply->code != 0) {
@@ -64,44 +58,24 @@ struct tnt_stream* get_new_key_by_reply (struct tnt_stream* s, uint32_t space_id
     assert (s);
     assert (reply->data);
 
-    //printf ("From %s, %d\n", __func__, __LINE__);
-    
-
     uint32_t key_fields[MaxKeyFieldsInTuple];
     uint32_t fields_num = get_key_fields (s, key_fields, space_id);
 
-    //printf ("From %s, %d\n", __func__, __LINE__);
-    uint32_t byte_num = 0;
     char last_tuple[BUFSIZ]  = {};
     char new_key_buf[BUFSIZ] = {};
     const char* ptr = reply->data;
-    // printf ("From %s, %d\n", __func__, __LINE__);
-    //printf ("%p\n", reply->data);
+    
     int len = mp_decode_array (&ptr) - 1;
     if (len < 0) return NULL;
-    //printf ("len = %d\n", len);
-    // printf ("From %s, %d\n", __func__, __LINE__);
-    // const char* reply_p = reply->data;
-    // printf ("Reply is ");
-    // print_array (&reply_p, 0);
-    // printf ("\n");
-
+    
     get_subarr_from_mp_array (reply->data, last_tuple, &len, 1, NULL);
     assert (mp_typeof (*last_tuple) == MP_ARRAY);
-    // printf ("From %s, %d\n", __func__, __LINE__);
-    // const char* last_tuple_print = last_tuple;
-    // printf ("Last tuple is ");
-    // print_array (&last_tuple_print, 0);
-    // printf ("\n");
-
+    
     const char* last_tuple_p = last_tuple;
     mp_decode_array (&last_tuple_p);
+    uint32_t byte_num = 0;
     get_subarr_from_mp_array (last_tuple_p, new_key_buf, key_fields, fields_num, &byte_num);
 
     struct tnt_stream* new_key = tnt_object_as (NULL, new_key_buf, byte_num);
-
-    // const char* key_data = TNT_SBUF_DATA (new_key);
-    // print_element (&key_data, 0);
-    // printf ("\n\n");
     return new_key;
 }
