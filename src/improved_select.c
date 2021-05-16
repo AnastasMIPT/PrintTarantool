470ac5573f6d4f2103b9e0409746ea34843ceb9b
AnastasMIPT <AtosBel@ya.ru>
2021-05-17 00:56:24 +0300

    documentation improvements

--- src/improved_select.c
+++ src/improved_select.c
@@ -10,14 +10,15 @@ const uint32_t MaxKeyFieldsInTuple   = 10;
 
 
 
-/*! Function for efficient select slice from space. Slice bounded by key of first tuple and limit
+/*! Function for efficiently selecting a slice from space.
+*   The fragment is selected using the "GT" iterator by the "key" with a "limit" tuple constraint.
 *	\param stream     stream object 
-*	\param space_id   space no
+*	\param space_id   space's id
 *	\param limit      limit of tuples to select
-*	\param key        key object. Tuples will be selected with this key with GT iterator.
+*	\param key        key object. Tuples will be selected by this key using the "GT" iterator.
 *	\param reply      pointer to reply object for storing result
 *   
-*   \returns primary key from last tuple in reply
+*   \returns primary key from the last tuple from the reply or NULL if the reply is empty
 */
 struct tnt_stream* select_some_after_key (struct tnt_stream* s, uint32_t space_id,
                                             uint32_t limit, struct tnt_stream* key, struct tnt_reply* reply) {
@@ -43,13 +44,13 @@ struct tnt_stream* select_some_after_key (struct tnt_stream* s, uint32_t space_i
 
 
 
-/*! Function for efficient select first limit elements.
+/*! Function for efficient selection of the first 'limit' tuples.
 *	\param stream     stream object 
-*	\param space_id   space no
+*	\param space_id   space's id
 *	\param limit      limit of tuples to select
 *	\param reply      pointer to reply object for storing result
 *   
-*   \returns primary key from last tuple in reply
+*   \returns primary key from the last tuple from the reply or NULL if the reply is empty
 */
 struct tnt_stream* select_some_first (struct tnt_stream* s, uint32_t space_id, uint32_t limit,
                                                                         struct tnt_reply* reply) {
@@ -73,13 +74,13 @@ struct tnt_stream* select_some_first (struct tnt_stream* s, uint32_t space_id, u
 }
 
 
-/*! Function getting last key from reply
+/*! Function to retrieve the primary key from the last reply tuple.
 *	\param stream     stream object 
-*	\param space_id   space no
+*	\param space_id   space's id
 *	\param limit      limit of tuples to select
 *	\param reply      pointer to reply object for storing result
 *   
-*   \returns primary key from last tuple in reply
+*   \returns primary key from the last tuple from the reply or NULL if the reply is empty
 */
 struct tnt_stream* get_new_key_by_reply (struct tnt_stream* s, uint32_t space_id, struct tnt_reply* reply) {
     assert (reply);
